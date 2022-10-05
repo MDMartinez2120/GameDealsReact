97b0eef10b78a31c2f63e7b1803e07139dc3bbe0
Matthew Martinez <matt.martinez2120@gmail.com>
2022-10-05 09:54:52 -0500

    made a placeholder carousel component.

--- src/Components/GameBanner.js
+++ src/Components/GameBanner.js
@@ -1,68 +1,57 @@
 import React from "react";
 import './GameBanner.css'
 
-function GameBanner(){
-//     let slideIndex = 1;
-//     showSlides(slideIndex);
-//
-// // Next/previous controls
-//     function plusSlides(n) {
-//         showSlides(slideIndex += n);
-//     }
-//
-// // Thumbnail image controls
-//     function currentSlide(n) {
-//         showSlides(slideIndex = n);
-//     }
-//
-//     function showSlides(n) {
-//         let i;
-//         let slides = document.getElementsByClassName("mySlides");
-//         let dots = document.getElementsByClassName("dot");
-//         if (n > slides.length) {slideIndex = 1}
-//         if (n < 1) {slideIndex = slides.length}
-//         for (i = 0; i < slides.length; i++) {
-//             slides[i].style.display = "none";
-//         }
-//         for (i = 0; i < dots.length; i++) {
-//             dots[i].className = dots[i].className.replace(" active", "");
-//         }
-//         slides[slideIndex-1].style.display = "block";
-//         dots[slideIndex-1].className += " active";
-//     }
-    function submitHandler(e){
-        e.preventDefault();
+const GameBanner = () => {
+    const [index, setIndex] = React.useState(0);
+    const timeoutRef = React.useRef(null);
+    const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+    const delay = 2500;
+
+    const resetTimeout = () => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
     }
 
-    // return (
-    //         <div className='slideshow-container'>
-    //             <div className="mySlides fade">
-    //                 <div className="number-text">1 / 3</div>
-    //                 <img src="" style="width:100%"/>
-    //                     <div className="text">Caption Text</div>
-    //             </div>
-    //
-    //             <div className="mySlides fade">
-    //                 <div className="number-text">2 / 3</div>
-    //                 <img src="" style="width:100%"/>
-    //                     <div className="text">Caption Two</div>
-    //             </div>
-    //
-    //             <div className="mySlides fade">
-    //                 <div className="number-text">3 / 3</div>
-    //                 <img src="" style="width:100%"/>
-    //                     <div className="text">Caption Three</div>
-    //             </div>
-    //             <div>
-    //                 <a className="prev" onClick="plusSlides(-1)">&#10094;</a>
-    //                 <a className="next" onClick="plusSlides(1)">&#10095;</a>
-    //             </div>
-    //             <div style="text-align:center">
-    //                 <span className="dot" onClick={submitHandler}></span>
-    //                 <span className="dot" onClick={submitHandler}></span>
-    //                 <span className="dot" onClick={submitHandler}></span>
-    //             </div>
-    //         </div>
-    // )
+    React.useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+                ),
+            delay
+        );
+
+        return () => {
+            resetTimeout();
+        };
+    }, [index]);
+
+    return (
+        <div className="slideshow">
+            <div
+                className="slideshowSlider"
+                style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+                {colors.map((backgroundColor, index) => (
+                    <div
+    className="slide"
+    key={index}
+    style={{backgroundColor}}
+    />
+                ))}
+            </div>
+
+            <div className="slideshowDots">
+                {colors.map((_, idx) => (
+                    <div key={idx} className={`slideshowDot${index === idx ? " active" : ""}`} onClick={() => {
+    setIndex(idx);
+}}/>
+                ))}
+            </div>
+        </div>
+    );
 }
+
 export default GameBanner;
