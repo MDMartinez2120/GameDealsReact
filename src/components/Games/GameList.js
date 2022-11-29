3c367f9cb05869eefe3fcd386d05f6aca2693344
Matthew Martinez <matt.martinez2120@gmail.com>
2022-11-29 16:11:58 -0600

    added styling to game cards.

--- src/components/Games/GameList.js
+++ src/components/Games/GameList.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import './GameList.css';
 import Card from "../UI/Card/Card";
 
 const GameList = () => {
@@ -12,18 +13,24 @@ const GameList = () => {
         }
     };
 
-    fetch('https://cheapshark-game-deals.p.rapidapi.com/games?title=batman&exact=0&limit=8', options)
+    fetch('https://cheapshark-game-deals.p.rapidapi.com/games?title=batman&exact=0&limit=10', options)
         .then(response => response.json())
         .then(response => setGame(response))
-        .catch(err => console.error(err));
+        .catch(err => console.log(err));
 
     return (
         <div>
             {game.map(games => (
-                <div>
-                    <h4 className='game-title'>{games.external}</h4>
-                    <img src={games.thumb} alt="thumbnail"/>
-                </div>
+                    <div className='game-cards'>
+                        <img src={games.thumb} alt="game-image"/>
+                            <div className="container">
+                                <h4><b>{games.external}</b></h4>
+                                <p>{games.cheapest}</p>
+                            </div>
+                        {/*<h4 className='game-title'>{games.external}</h4>*/}
+                        {/*<img className='game-image' src={games.thumb} alt="thumbnail"/>*/}
+                        {/*<p className='game-price'>sale price: {games.cheapest}</p>*/}
+                    </div>
             ))}
         </div>
     )
